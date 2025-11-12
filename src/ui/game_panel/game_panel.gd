a5f9d4ab75deb7f6a8502f9db2c8a69779767612
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-12 19:55:19 +0100

    Add game panel

---
+++ src/ui/game_panel/game_panel.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_sentence_button_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+
+func _on_map_button_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
