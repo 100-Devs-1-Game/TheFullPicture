fbacf9fa9896d962d18943de82692ffe2058fd19
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-16 19:03:17 +0100

    Fix parsing

--- src/data/interactable_object_data.gd
+++ src/data/interactable_object_data.gd
@@ -7,9 +7,58 @@ extends Resource
 		clues_file= f
 		description= FileAccess.get_file_as_string(f)
 		
-
-## Interactable Objects can contain multiple objects, like a
-## Desk can contain a Pen. The Pen is only reachable via the Desk
-@export var container: Array[InteractableObjectData]
-
 var description: String
+
+
+
+func preparse_description():
+	print("Pre-parsing ", clues_file)
+	var result: String
+	result= description.replace("<clue>", "[url]")
+	result= result.replace("</clue>", "[/url]")
+
+	var splits:= result.split("[url]")
+	if splits.size() > 1:
+		for i in range(1, splits.size()):
+			var split: String= splits[i]
+			var word: String= split.left(split.find("[/"))
+			split= split.insert(0, "[url=%s]" % word.to_lower())
+			splits[i]= split
+
+		result= "".join(splits) 
+	
+	print(result)
+	print()
+	description= result
+
+
+func parse()-> String:
+	prints("Parse", clues_file)
+	
+	var result:= description
+
+	var splits:= result.split("[url")
+	if splits.size() > 1:
+		for i in range(1, splits.size()):
+			var split: String= splits[i]
+
+			var key_idx:= split.find("=")
+			var close_tag_idx:= split.find("]")
+			var key:= split.substr(key_idx + 1, close_tag_idx - key_idx - 1)
+			prints(" found key", key)
+			assert(GameData.clues.has(key))
+			var clue: ClueData= GameData.clues[key]
+			if clue.discovered:
+				split= split.replace(split.left(close_tag_idx), "[color=blue")
+				split= split.replace("[/url]", "[/color]")
+			else:
+				split= split.insert(split.find("]") + 1, "[color=red]")
+				split= split.insert(0, "[url")
+				split= split.insert(split.find("[/url]"), "[/color]")
+				
+			splits[i]= split
+			
+		result= "".join(splits) 
+	
+	print(result)
+	return result
