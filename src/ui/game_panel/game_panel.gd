59deaf19afd1d44598c6158c2b1a86961cb58fdb
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-16 19:03:45 +0100

    Update game panel

--- src/ui/game_panel/game_panel.gd
+++ src/ui/game_panel/game_panel.gd
@@ -6,8 +6,10 @@ func _on_settings_button_pressed() -> void:
 
 
 func _on_sentence_button_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
+	if toggled_on:
+		SceneLoader.goto_sentence_builder()
 
 
 func _on_map_button_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
+	if toggled_on:
+		SceneLoader.goto_map()
