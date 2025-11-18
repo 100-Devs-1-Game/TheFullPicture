be010c3a5a037963d13d74c2ee67012cfd44c9e0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-18 02:03:48 +0100

    Generate checkboxes dynamically

--- src/ui/game_panel/game_panel.gd
+++ src/ui/game_panel/game_panel.gd
@@ -1,6 +1,25 @@
 extends CanvasLayer
 
+@export var checkbox_scene: PackedScene
+
 @onready var map_button: TextureButton = %"Map Button"
+@onready var checkbox_grid: GridContainer = %"Checkbox GridContainer"
+
+
+
+func _ready() -> void:
+	EventManager.entered_next_stage.connect(update_check_boxes)
+	update_check_boxes()
+
+
+func update_check_boxes():
+	for child in checkbox_grid.get_children():
+		checkbox_grid.remove_child(child)
+		child.queue_free()
+
+	for i in GameData.current_stage.clues.size():
+		var child= checkbox_scene.instantiate()
+		checkbox_grid.add_child(child)
 
 
 func _on_settings_button_pressed() -> void:
