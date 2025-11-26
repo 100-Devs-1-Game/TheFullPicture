7010297ba6677ca2b13021b306f813bbe73e6997
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-11-26 03:06:35 +0100

    Hide quit button in web export

--- src/menus/main_menu.gd
+++ src/menus/main_menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
 @onready var settings_popup: SettingsPopup = $SettingsPopup
+@onready var quit_button: TextureButton = $VBoxContainer/QuitButton
 
 
 
 func _ready() -> void:
+	if OS.get_name() == "Web":
+		quit_button.hide()
+	
 	settings_popup.hide()
 	MusicPlayer.play_menu()
 
