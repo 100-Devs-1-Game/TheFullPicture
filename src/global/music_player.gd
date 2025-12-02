d1bdc3f9b4394b395bf405ee00f2d19c20141219
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-12-02 02:17:27 +0100

    Remove music fading to fix overlapping music bug

--- src/global/music_player.gd
+++ src/global/music_player.gd
@@ -3,29 +3,39 @@ extends Node
 const FADE_DURATION= 1.0
 
 var players: Array[AudioStreamPlayer]
+var tweens: Dictionary[AudioStreamPlayer, Tween]
 
 
 
 func _ready() -> void:
 	for child in get_children():
 		players.append(child)
+		#tweens[child as AudioStreamPlayer]= null
 
 
 func play(player: AudioStreamPlayer):
-	if player.playing:
-		return
-	
-	for other_player in players:
-		if other_player.playing:
-			var fade_out:= create_tween()
-			fade_out.tween_property(other_player, "volume_linear", 0.0, FADE_DURATION)
-			fade_out.tween_callback(other_player.stop)
-			break
-
-	player.volume_linear= 0
-	var fade_in:= create_tween()
+	for every_player in players:
+		every_player.stop()
 	player.play()
-	fade_in.tween_property(player, "volume_linear", 1.0, FADE_DURATION)
+
+	#if player.playing:
+		#return
+	#
+	#for other_player in players:
+		#if other_player != player and other_player.playing:
+			#if tweens[other_player] and tweens[other_player].is_running():
+				#tweens[other_player].kill()
+			#tweens[other_player]= create_tween()
+			#tweens[other_player].tween_property(other_player, "volume_linear", 0.0, FADE_DURATION)
+			#tweens[other_player].tween_callback(other_player.stop)
+			#break
+#
+	#player.volume_linear= 0
+	#if tweens[player] and tweens[player].is_running():
+		#tweens[player].kill()
+	#tweens[player]= create_tween()
+	#player.play()
+	#tweens[player].tween_property(player, "volume_linear", 1.0, FADE_DURATION)
 
 
 func play_menu():
